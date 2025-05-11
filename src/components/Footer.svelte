663c211375e0b3a36913088647e05ad5f111e449
Tavaropolis <tavares.ag.gabriel@gmail.com>
2025-05-11 11:16:40 -0300

    feat: about page added

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,4 +2,19 @@
     <address>
         &#169Gabriel Tavares, <a href="http://github.com/Tavaropolis" target="_blank">github.com/Tavaropolis</a>, 2025
     </address>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
 </footer>
+
+<style>
+ul li {
+    display: inline;
+    margin: 2px;
+}
+
+a {
+    color: white
+}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+//Components
+import Footer from "../../components/Footer.svelte"
+
+</script>
+<div class="about-page">
+    <main>
+        <h1>About the project</h1>
+        <p>This project is a remake of my first project made at the beginning of my journey learning programming, back in 2022.  You can check out the original "Battle against the Monster" <a href="https://tavaropolis.github.io/luta-contra-monstro-vue/" target="_blank">here</a>.</p>
+        <img src="/oldapp.png" alt="">
+        <p>The idea here is to see how much I have been improving in these last 3 years. This time, I tried to make the game more challenging and more visually pleasing, with character sprites and animations.</p>
+        <p>Feel free to contact me via: tavaropolisgithub@gmail.com</p>
+    </main>
+    <Footer/>
+</div>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+.about-page {
+    width: 100%;
+    min-height: 100vh;
+    background-color: black;
+    color: yellow;
+    font-family: runescape;
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;   
+    overflow-x: hidden;
+}
+
+main {
+    flex: 1;
+}
+
+main h1 {
+    font-size: 3rem;
+}
+
+main p {
+    font-size: 1.2rem;
+}
+
+img {
+    width: 70%;
+    height: auto;
+}
+</style>
