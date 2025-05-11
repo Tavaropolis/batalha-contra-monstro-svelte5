25a57faaeb0c3b5a4689d3809a313c2d9d328755
Tavaropolis <tavares.ag.gabriel@gmail.com>
2025-05-11 14:33:13 -0300

    test: test for fixing deploy bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -414,6 +414,11 @@ $effect(() => {
 </div>
 
 <style>
+@font-face {
+  font-family: "runescape";
+  src: url(../../fonts/runescape_uf.woff) format('woff');
+}
+
 * {
     box-sizing: border-box;
 }
